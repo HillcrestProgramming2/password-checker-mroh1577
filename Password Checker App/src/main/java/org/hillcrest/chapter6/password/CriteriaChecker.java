e31b8ce25d93069a3125b51bba22f625d2d58b32
mroh1577 <rohilamaahi@gmail.com>
2026-01-06 08:54:39 -0700

    added some code

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -3,4 +3,21 @@ package org.hillcrest.chapter6.password;
 public class CriteriaChecker {
 
     //utility class
+    public static int evaluateCriteria(String password){
+        if (password == null ) {
+            return 0;
+        }
+        int count = 0;
+        boolean hasAnUpperCase = false;
+        boolean hasALowerCase = false;
+        boolean hasAtLeastOneNumber = false;
+        boolean hasASpecialCharacter = false;
+        final String specialCharacters = "!@#$%^&*()-+=";
+
+        if(password.length() >= 8){
+           count ++;
+        }
+
+    }
+
 }
--- Password Checker App/src/test/java/org/hillcrest/chapter6/password/FeedbackGeneratorTest.java
+++ Password Checker App/src/test/java/org/hillcrest/chapter6/password/FeedbackGeneratorTest.java
@@ -1,7 +1,9 @@
 package org.hillcrest.chapter6.password;
 
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FeedbackGeneratorTest {
+
 
 }
